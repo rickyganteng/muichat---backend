f22cfdffc8c0a84a67af0cacb73cdfd60458fab9
rickyganteng <syahputraricky2@gmail.com>
2021-09-24 12:46:12 +0700

    revisi

--- src/middleware/redis.js
+++ src/middleware/redis.js
@@ -1,6 +1,10 @@
 
 const redis = require('redis')
-const client = redis.createClient()
+const client = redis.createClient({
+  host: process.env.REDIS_HOSTNAME,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD
+})
 const helper = require('../helpers/wrapper')
 
 module.exports = {
